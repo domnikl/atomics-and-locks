b3c1babfea98d30a3c6edb83ecfb28108ae5328c
Dominik Liebler <liebler.dominik@gmail.com>
2023-03-20 15:58:25 +0100

    mutex example

---
+++ 5-mutex/src/main.rs
@@ -0,0 +1,22 @@
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let n = Mutex::new(0);
+
+    thread::scope(|s| {
+        for _ in 0..10 {
+            s.spawn(|| {
+                let mut guard = n.lock().unwrap();
+                for _ in 0..100 {
+                    *guard += 1;
+                }
+                thread::sleep(Duration::from_secs(1));
+            });
+        }
+    });
+
+    // into_inner removes the protection
+    assert_eq!(n.into_inner().unwrap(), 1000);
+}
