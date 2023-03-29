25b3b880a5a170653e2f21d9bd6f3b6ab801f0bf
Dominik Liebler <liebler.dominik@gmail.com>
2023-03-29 13:37:40 +0200

    add spin lock implementation

---
+++ src/13-spink-lock.rs
@@ -0,0 +1,74 @@
+use std::cell::UnsafeCell;
+use std::ops::{Deref, DerefMut};
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::{Acquire, Release};
+use std::thread;
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self { locked: AtomicBool::new(false), value: UnsafeCell::new(value) }
+    }
+
+    pub fn lock<'a>(&'a self) -> Guard<T> {
+        // TODO: change it to use compare-and-exchange instead of swap
+        while self.locked.swap(true, Acquire) {
+            // tells the processor that we're spinning while waiting for something to change
+            std::hint::spin_loop();
+            // might be useful to spin_loop several times, but highly depends on hardware
+        }
+
+        Guard { lock: self }
+    }
+}
+
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        // Safety: the very existence of this Guard
+        // guarantees we've exclusively locked the lock
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Release);
+    }
+}
+
+fn main() {
+    let x = SpinLock::new(Vec::new());
+
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(3);
+        });
+    });
+
+    let mut g = x.lock();
+
+    g.sort();
+
+    assert!(g.as_slice() == [1, 2, 3]);
+}
