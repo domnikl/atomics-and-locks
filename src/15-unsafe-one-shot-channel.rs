0ee0ef2e83764221fd6e8309d11f17483cb6e7f7
Dominik Liebler <liebler.dominik@gmail.com>
2023-04-05 08:29:58 +0200

    implement blocking

--- src/15-unsafe-one-shot-channel.rs
+++ src/15-unsafe-one-shot-channel.rs
@@ -1,8 +1,9 @@
 use std::cell::UnsafeCell;
+use std::marker::PhantomData;
 use std::mem::MaybeUninit;
 use std::sync::atomic::AtomicU8;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
-use std::thread;
+use std::thread::{self, Thread};
 
 const EMPTY: u8 = 0;
 const WRITING: u8 = 1;
@@ -11,6 +12,7 @@ const READING: u8 = 2;
 
 pub struct Sender<'a, T> {
     channel: &'a Channel<T>,
+    receiving_thread: Thread,
 }
 
 impl<T> Sender<'_, T> {
@@ -26,26 +28,22 @@ impl<T> Sender<'_, T> {
 
         unsafe { (*self.channel.message.get()).write(message) };
         self.channel.state.store(READY, Release);
+        self.receiving_thread.unpark();
     }
 }
 
 pub struct Receiver<'a, T> {
     channel: &'a Channel<T>,
+    _no_send: PhantomData<*const ()>, // disallow sending the Receiver between threads, `*const ()`
+    // is a raw pointer, that does not implement Send
 }
 
 impl<T> Receiver<'_, T> {
-    pub fn is_ready(&self) -> bool {
-        self.channel.state.load(Relaxed) == READY
-    }
-
-    pub fn receive(self) -> T {
-        if self
-            .channel
-            .state
-            .compare_exchange(READY, READING, Acquire, Relaxed)
-            .is_err()
-        {
-            panic!("no message available");
+   pub fn receive(self) -> T {
+        // thread::park() might return spuriously, so the loop needs to be there to check if we
+        // still need to block
+        while self.channel.state.compare_exchange(READY, READING, Acquire, Relaxed).is_err()  {
+            thread::park();
         }
 
         unsafe { (*self.channel.message.get()).assume_init_read() }
@@ -71,7 +69,7 @@ impl<T> Channel<T> {
 
     pub fn split(&mut self) -> (Sender<T>, Receiver<T>) {
         *self = Self::new();
-        (Sender { channel: self }, Receiver { channel: self })
+        (Sender { channel: self, receiving_thread: thread::current() }, Receiver { channel: self, _no_send: PhantomData })
     }
 }
 
@@ -88,16 +86,10 @@ fn main() {
 
     thread::scope(|s| {
         let (sender, receiver) = channel.split();
-        let t = thread::current();
 
         s.spawn(move || {
             sender.send("hello world!");
-            t.unpark();
         });
-
-        while !receiver.is_ready() {
-            thread::park();
-        }
 
         assert_eq!(receiver.receive(), "hello world!");
     });
